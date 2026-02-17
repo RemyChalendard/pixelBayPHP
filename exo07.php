8139e25778749aa57d8a99e1a616017280c22365
RemyChalendard <chalendard.remy@gmail.com>
2026-02-17 11:26:27 +0100

    exo07

--- exo07.php
+++ exo07.php
@@ -7,34 +7,27 @@ $collection = [
         "stock" => 40,
         "scores" => [85, 89, 88, 92, 89]
     ],
-    
-
-    // Ajoutez au moins 3 autres jeux
-
     [
         "titre" => "The last of us",
         "prix" => 39.99,
-        "genre" => "aventure",
+        "genre" => "Aventure",
         "stock" => 30,
         "scores" => [90, 89, 92, 88, 89]
     ],
-
     [
-        "titre" => "Zelda breath of the wild",
+        "titre" => "Zelda Breath of the Wild",
         "prix" => 49.99,
-        "genre" => "aventure",
+        "genre" => "Aventure",
         "stock" => 20,
         "scores" => [88, 89, 93, 90, 89]
     ],
-
     [
-        "titre" => "God of war ragnarock",
+        "titre" => "God of War Ragnarock",
         "prix" => 49.99,
-        "genre" => "aventure",
+        "genre" => "Aventure",
         "stock" => 20,
         "scores" => [87, 91, 92, 89, 89]
     ],
-
     [
         "titre" => "Flight Sim",
         "prix" => 49.99,
@@ -42,18 +35,21 @@ $collection = [
         "stock" => 20,
         "scores" => [91, 91, 89, 94, 90]
     ],
-
 ];
 
-// Vos fonctions ici
+function moyenneScores($jeu) {
+    if ($jeu['scores']) {
+    }
+    return array_sum($jeu['scores']) / count($jeu['scores']);
+}
 
 function afficherJeu($jeu)
 {
     echo "<br>Titre : " . $jeu['titre'] . "<br>";
-    echo " Prix :  " . $jeu['prix'] . "<br>";
-    echo " Genre : " . $jeu['genre'] . "<br>";
-    echo " Stock : " . $jeu['stock'] . "<br>";
-    echo " Score : " . ($jeu['scores'] ?? "") . "<br>";
+    echo "Prix : " . $jeu['prix'] . "<br>";
+    echo "Genre : " . $jeu['genre'] . "<br>";
+    echo "Stock : " . $jeu['stock'] . "<br>";
+    echo "Score moyen : " . (moyenneScores($jeu)) . "<br>";
 }
 
 echo "Jeux Pixel Bay<br>";
@@ -61,12 +57,6 @@ foreach ($collection as $jeu) {
     afficherJeu($jeu);
 }
 
-function moyenneScores($jeu) {
-    foreach ($jeu['scores'] as $score) {
-        $somme += $score;
-    }
-    return array_sum($jeu['scores']) / count($jeu['scores']);
-}
 
 
 /*
